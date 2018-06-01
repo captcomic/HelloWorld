ca016e12ff7ff51bfeaef32dd58b879ee37a1e98
Yeashfi Hasan <capt.comic@gmail.com>
2018-05-31 20:14:10 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Fariha on 5/31/18.
+//  Copyright © 2018 Bootcamp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
